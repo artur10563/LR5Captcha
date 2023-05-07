d2ce1c6d3b8bc537244e74ece9384701eb3ec325
artur10563 <galkaartur2004galkaartur@gmail.com>
2023-05-07 16:21:49 +0300

    Добавьте файлы проекта.

---
+++ LR5Captcha/EquationGenerator.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR5Captcha
+{
+    internal static class EquationGenerator
+    {
+        private enum Operation
+        {
+            Multiplication,
+            Addition,
+            Subtraction,
+            Division
+        }
+
+
+        private static Random random = new Random();
+        private static List<double> GenerateNumSet(int K)
+        {
+            List<double> nums = new List<double>();
+            int randint;
+            double randouble;
+
+            for (int i = 0; i < K; i++)
+            {
+                randint = random.Next(0, 100);
+                randouble = 0;
+
+                if (random.Next(0, 2) == 0)
+                {
+                    randouble = Math.Round(random.NextDouble(), 2);
+                }
+
+                nums.Add(randint + randouble);
+            }
+            return nums;
+        }
+
+
+        public static string Generate(int K)
+        {
+            StringBuilder sb = new StringBuilder();
+            Operation randoper;
+            foreach (double d in GenerateNumSet(K))
+            {
+                sb.Append(d);
+                randoper = (Operation)random.Next(Operation.GetValues(typeof(Operation)).Length);
+                switch (randoper)
+                {
+                    case Operation.Multiplication:
+                        sb.Append(" * ");
+                        break;
+                    case Operation.Addition:
+                        sb.Append(" + ");
+                        break;
+                    case Operation.Subtraction:
+                        sb.Append(" - ");
+                        break;
+                    case Operation.Division:
+                        sb.Append(" / ");
+                        break;
+                }
+            }
+            return sb.ToString().Trim().Remove(sb.Length - 2);
+        }
+
+        public static string Evaluate(string equation)
+        {
+            DataTable dt = new DataTable();
+
+            equation = equation.Replace(",", ".");
+
+            double res = Convert.ToDouble(dt.Compute(equation, ""));
+            return res.ToString("0.00");
+        }
+    }
+}
---
+++ LR5Captcha/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LR5Captcha
+{
+    public partial class Form1 : Form
+    {
+
+        private void NewCaptcha()
+        {
+            int k = random.Next(min, max);
+            label1.Text = EquationGenerator.Generate(k);
+            this.Text = EquationGenerator.Evaluate(label1.Text);
+        }
+
+        private static Random random = new Random();
+        private static int min = 3;
+        private static int max = 7;
+        public Form1()
+        {
+            InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.Text = "Captcha";
+            NewCaptcha();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string input = textBox1.Text;
+            if (input.CompareTo(EquationGenerator.Evaluate(label1.Text)) == 0)
+            {
+                MessageBox.Show("Captcha solved!");
+                Environment.Exit(0);
+            }
+            else
+            {
+                textBox1.Text = string.Empty;
+                NewCaptcha();
+            }
+        }
+    }
+}
